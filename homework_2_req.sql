d93f6f972333913d8d391af91a5dc38612c7771f
titronius <titronius@gmail.com>
2024-04-30 16:19:23 +0300

    fix

--- homework_2_req.sql
+++ homework_2_req.sql
@@ -1,45 +1,45 @@
-CREATE TABLE musis_genres (
+CREATE TABLE IF NOT EXISTS musis_genres (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE musicians (
+CREATE TABLE IF NOT EXISTS musicians (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE genre_musician (
+CREATE TABLE IF NOT EXISTS genre_musician (
     id SERIAL PRIMARY KEY,
     music_genre_id INTEGER NOT NULL REFERENCES musis_genres(id),
     musician_id INTEGER NOT NULL REFERENCES musicians(id)
 );
 
-CREATE TABLE albums (
+CREATE TABLE IF NOT EXISTS albums (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
-    year_of_release INTEGER(4) CHECK (year_of_release >= 1649)
+    year_of_release INTEGER CHECK (year_of_release >= 1649)
 );
 
-CREATE TABLE musician_album (
+CREATE TABLE IF NOT EXISTS musician_album (
     id SERIAL PRIMARY KEY,
     musician_id INTEGER NOT NULL REFERENCES musicians(id),
-    album_id INTEGER NOT NULL REFERENCES albums(id),
+    album_id INTEGER NOT NULL REFERENCES albums(id)
 );
 
-CREATE TABLE songs (
+CREATE TABLE IF NOT EXISTS songs (
     id SERIAL PRIMARY KEY,
     album_id INTEGER NOT NULL REFERENCES albums(id),
     name VARCHAR(60) NOT NULL,
     duration INTEGER CHECK (duration <= 48212)
 );
 
-CREATE TABLE collections (
+CREATE TABLE IF NOT EXISTS collections (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
-    year_of_release INTEGER(4) CHECK (year_of_release >= 1649)
+    year_of_release INTEGER CHECK (year_of_release >= 1649)
 );
 
-CREATE TABLE song_collection (
+CREATE TABLE IF NOT EXISTS song_collection (
     id SERIAL PRIMARY KEY,
     song_id INTEGER NOT NULL REFERENCES songs(id),
     collection_id INTEGER NOT NULL REFERENCES collections(id)
